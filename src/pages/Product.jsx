87722bbcabdf4232be32b703c74d2d1d72c07aeb
8Lou <lou8@mail.ru>
2023-05-14 18:47:43 +0300

    поиск_проблемы_отображения_карточки товара_безрез

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,47 +5,34 @@ import { Button } from "react-bootstrap";
 import { Back } from "react-bootstrap-icons";
 
 const Product = () => {
-  const { id } = useParams(); //передается _id свойство товара из базы данных
-  const [data, setData] = useState({}); //стэйт в квадратные скобки
+	const { id } = useParams()
+	const [data, setData] = useState({});
 
-  useEffect(() => {
-    fetch(`https://api.react-learning.ru/products/${id}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token12")}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((serverData) => {
-        /* console.log(id, serverData); */
-        setData(serverData);
-      });
-  }, [id]); //квадратные внутри круглых - точка останова - useEffect выполнить 1 раз
+	useEffect(() => {
+		fetch(`https://api.react-learning.ru/products/${id}`, {
+			headers: {
+				"Authorization": `Bearer ${localStorage.getItem("token12")}`
+			}
+		})
+			.then(res => res.json())
+			.then(serverData => {
+				
+				setData(serverData);
+			})
+	}, [id]) //квадратные внутри круглых - точка останова - useEffect выполнить 1 раз
 
-  return (
-    <>
-      <Link to={`/catalog#pro_${id}`}>Назад</Link>
-      {data.name ? (
-        <>
-          <h1>{data.name}</h1>
-          <img src={data.pictures} alt={data.name} />
-        </>
-      ) : (
-        <div className="info" style={{ textAlign: "center" }}>
-          Товара {id} не существует
-          <br />
-          или
-          <br />
-          он еще не загружен
-        </div>
-      )}
-    </>
-  );
-};
+return <>
+		<Link to={`/catalog#pro_${id}`}>Назад</Link>
+		{data.name 
+			? <>
+				<h1>{data.name}</h1>
+				<img src={data.pictures} alt={data.name} />
+			</> 
+			: <div className="info" style={{textAlign: "center"}}>
+				Товара {id} не существует<br/>или<br/>он еще не загружен
+			</div>
+		}
+	</>
+}
 
 export default Product;
-
-{
-  /* <Link to="*">
-          <ErrorPage />
-        </Link> */
-}
